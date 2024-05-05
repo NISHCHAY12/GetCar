1b4fb1c12667b0917f53e42a5a49fdff267ca838
NISHCHAY12 <87168276+NISHCHAY12@users.noreply.github.com>
2024-05-05 17:44:36 +0530

    Add files via upload

---
+++ page.php
@@ -0,0 +1,123 @@
+<?php
+session_start();
+error_reporting(0);
+include('includes/config.php');
+?>
+
+<!DOCTYPE HTML>
+<html lang="en">
+<head>
+
+<title>Car Rental Portal | Page details</title>
+<!--Bootstrap -->
+<link rel="stylesheet" href="assets/css/bootstrap.min.css" type="text/css">
+<!--Custome Style -->
+<link rel="stylesheet" href="assets/css/style.css" type="text/css">
+<!--OWL Carousel slider-->
+<link rel="stylesheet" href="assets/css/owl.carousel.css" type="text/css">
+<link rel="stylesheet" href="assets/css/owl.transitions.css" type="text/css">
+<!--slick-slider -->
+<link href="assets/css/slick.css" rel="stylesheet">
+<!--bootstrap-slider -->
+<link href="assets/css/bootstrap-slider.min.css" rel="stylesheet">
+<!--FontAwesome Font Style -->
+<link href="assets/css/font-awesome.min.css" rel="stylesheet">
+
+<!-- Fav and touch icons -->
+<link rel="apple-touch-icon-precomposed" sizes="144x144" href="assets/images/favicon-icon/apple-touch-icon-144-precomposed.png">
+<link rel="apple-touch-icon-precomposed" sizes="114x114" href="assets/images/favicon-icon/apple-touch-icon-114-precomposed.html">
+<link rel="apple-touch-icon-precomposed" sizes="72x72" href="assets/images/favicon-icon/apple-touch-icon-72-precomposed.png">
+<link rel="apple-touch-icon-precomposed" href="assets/images/favicon-icon/apple-touch-icon-57-precomposed.png">
+<link rel="shortcut icon" href="assets/images/favicon-icon/favicon.png">
+<link href="https://fonts.googleapis.com/css?family=Lato:300,400,700,900" rel="stylesheet">
+</head>
+<body>
+        
+<!--Header-->
+<?php include('includes/header.php');?>
+
+<section class="page-header aboutus_page">
+  <div class="container">
+    <div class="page-header_wrap">
+      <div class="page-heading">
+        <h1>About Us</h1>
+      </div>
+    </div>
+  </div>
+  <!-- Dark Overlay-->
+  <div class="dark-overlay"></div>
+</section>
+<section class="about_us section-padding">
+  <div class="container">
+    <div class="section-header text-center">
+
+
+    <div class="details" align="center">
+        <h2 style="margin-bottom: 20px;">Team Details</h2>
+        <div id="cent">
+            <span class="mem">
+                <img class="im" src="./assets/images/prf.png" alt="kunal">
+                <h3>Kunal Bhamoriya</h3>
+                <p>(UI21CS32)</p>
+            </span>
+            <span class="mem">
+                <img class="im" src="./assets/images/prf.png" alt="nishchay">
+                <h3>Nishchay Singh</h3>
+                <p>(UI21CS36)</p>
+            </span>
+            <span class="mem">
+                <img class="im" src="./assets/images/prf.png" alt="akash">
+                <h3>Swaroop Debnath</h3>
+                <p>(UI21CS14)</p> 
+            </span>
+        </div>
+    </div>
+
+
+
+
+      <h5 style="font-size: 14px;"><span style="color:#00bba8;font-weight:600;">We offer a varied fleet of cars, ranging from the compact. All our vehicles have air conditioning,power steering, electric windows. All our vehicles are bought and maintained at official dealerships only. Automatic transmission cars are available in every booking class . </span><span style="color: rgb(52, 52, 52); font-family: Arial, Helvetica, sans-serif;">As we are not affiliated with any specific automaker, we are able to provide a variety of vehicle makes and models for customers to rent.</span>
+      <div><span style="color: rgb(62, 62, 62); font-family: &quot;Lucida Sans Unicode&quot;, &quot;Lucida Grande&quot;, sans-serif; font-size: 13px;">Our mission is to be recognised as the global leader in Car Rental for companies and the public and private sector by partnering with our clients to provide the best and most efficient Cab Rental solutions and to achieve service excellence.</h5>
+    </div>
+   
+  </div>
+</section>
+<!-- /About-us--> 
+
+
+
+
+
+<<!--Footer -->
+<?php include('includes/footer.php');?>
+<!-- /Footer--> 
+
+<!--Back to top-->
+<div id="back-top" class="back-top"> <a href="#top"><i class="fa fa-angle-up" aria-hidden="true"></i> </a> </div>
+<!--/Back to top--> 
+
+<!--Login-Form -->
+<?php include('includes/login.php');?>
+<!--/Login-Form --> 
+
+<!--Register-Form -->
+<?php include('includes/registration.php');?>
+
+<!--/Register-Form --> 
+
+<!-- Scripts --> 
+<script src="assets/js/jquery.min.js"></script>
+<script src="assets/js/bootstrap.min.js"></script> 
+<script src="assets/js/interface.js"></script> 
+<!--Switcher-->
+<script src="assets/switcher/js/switcher.js"></script>
+<!--bootstrap-slider-JS--> 
+<script src="assets/js/bootstrap-slider.min.js"></script> 
+<!--Slider-JS--> 
+<script src="assets/js/slick.min.js"></script> 
+<script src="assets/js/owl.carousel.min.js"></script>
+
+</body>
+
+<!-- Mirrored from themes.webmasterdriver.net/carforyou/demo/about-us.html by HTTrack Website Copier/3.x [XR&CO'2014], Fri, 16 Jun 2017 07:26:12 GMT -->
+</html>
